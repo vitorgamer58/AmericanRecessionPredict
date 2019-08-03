fae1afb6a93456a529b157918d8a31afc59ea52b
Vitor Araujo <vitorgamer58@outlook.com>
2019-08-03 14:58:25 -0300

    Update Python 3.X
    O Pacote FREDAPI está disponível no Python 3

--- crise.py
+++ crise.py
@@ -1,4 +1,4 @@
-#python 2.7
+#python 3.x
 #criado por vitorgamer58
 from fredapi import Fred 
 fred = Fred(api_key='sua api key aqui')
@@ -17,39 +17,39 @@ var30years = (taxa30years[-1])
 
 #com base nas taxas de juros da os resultados
 if(var3month>=var2years):
-	print 'A taxa de juros de 3 meses esta maior que a taxa de 2 anos'
+	print ('A taxa de juros de 3 meses esta maior que a taxa de 2 anos')
 
 if(var3month>=var10years):
-	print 'a taxa de 3 meses esta maior que a taxa de juros de 10 anos, cuidado'
+	print ('a taxa de 3 meses esta maior que a taxa de juros de 10 anos, cuidado')
 
 if(var2years>=var10years):
-	print 'a taxa de 2 anos esta maior que a taxa de 10 anos'
-	print 'sendo assim, tambem e um indicador de recessao'
-	print 'porem Anthony P. Geller prefere analisar as taxas de 3 meses e 30 anos'
+	print ('a taxa de 2 anos esta maior que a taxa de 10 anos')
+	print ('sendo assim, tambem e um indicador de recessao')
+	print ('porem Anthony P. Geller prefere analisar as taxas de 3 meses e 30 anos')
 	if(var3month<=var30years):
-		print ''
-		print 'e a taxa de juros de 3 meses ainda nao esta maior que a taxa de 30 anos'
-		print 'por enquanto, stay safe'
+		print ('')
+		print ('e a taxa de juros de 3 meses ainda nao esta maior que a taxa de 30 anos')
+		print ('por enquanto, stay safe')
 
 if(var3month>=var30years):
-	print 'agora fudeu'
-	print 'segundo a teoria devemos esperar por uma recessao em 6 ou 12 meses'
+	print ('agora fudeu')
+	print ('segundo a teoria devemos esperar por uma recessao em 6 ou 12 meses')
 
 if(var3month<=var30years):
-	print 'por enquanto esta tudo tranquilo com as taxas de 3 meses e 30 anos'
+	print ('por enquanto esta tudo tranquilo com as taxas de 3 meses e 30 anos')
 
 if(var3month>=var30years) or (var2years>=var10years) or (var3month>=var2years):
-	print ''
-	print 'Ground Control to Major Tom'
-	print 'Your circuits dead, theres something wrong'
-	print 'Can you hear me, Major Tom?'
-	print ''
-	print 'A chapa esta esquentando'
+	print ('')
+	print ('Ground Control to Major Tom')
+	print ('Your circuits dead, theres something wrong')
+	print ('Can you hear me, Major Tom?')
+	print ('')
+	print ('A chapa esta esquentando')
 
 
-print ''
-print 'taxas de juros:'
-print '3 Meses:', var3month
-print '2 Anos: ', var2years
-print '10 Anos:', var10years
-print '30 Anos: ', var30years
+print ('')
+print ('taxas de juros:')
+print ('3 Meses:', var3month)
+print ('2 Anos: ', var2years)
+print ('10 Anos:', var10years)
+print ('30 Anos: ', var30years)
